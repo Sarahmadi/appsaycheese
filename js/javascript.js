524684f7d0c4317775e2dbac5e714140c4ee00e9
Sarahmadi <95757345+Sarahmadi@users.noreply.github.com>
2022-01-27 12:24:24 +0100

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,12 @@
-if('serviceWorker' in navigator){
+if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register("/sw.js")
-		.then(function(){
+		.then(function () {
 			console.log("Service worker is registered!")
 		})
 }
 
-function displayFirstNotification(){
+function displayFirstNotification() {
 	$("#btnEnableNotifications").hide();
 
 
@@ -14,20 +14,20 @@ function displayFirstNotification(){
 		body: "You are successfully subscribed to Urban Connect notifications! Thank you very much!",
 		icon: "/images/icons/app_icon48x48.png",
 		// Star Wars shamelessly taken from Peter Beverloo
-      	// https://tests.peter.sh/notification-generator/
-		vibrate: [500,110,500,110,450,110,200,110,170,40,450,110,200,110,170,40,500]
-		
+		// https://tests.peter.sh/notification-generator/
+		vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40, 500]
+
 	}
 
-	navigator.serviceWorker.ready.then(function(registration){
+	navigator.serviceWorker.ready.then(function (registration) {
 		registration.showNotification("Successfully subscribed!!", options)
 	})
 }
 
-function askForNotificationPermission(){
-	Notification.requestPermission(function(result){
+function askForNotificationPermission() {
+	Notification.requestPermission(function (result) {
 		console.log("user Choice", result);
-		if(result != "granted"){
+		if (result != "granted") {
 			console.log("No permission granted");
 		} else {
 			console.log("Permission granted!!!");
@@ -37,29 +37,29 @@ function askForNotificationPermission(){
 	})
 }
 
-$(function(){
-	
-	if('Notification' in window){
+$(function () {
+
+	if ('Notification' in window) {
 		$("#btnEnableNotifications").show();
 		$("#btnEnableNotifications").click(askForNotificationPermission);
 	}
-	
+
 })
 
-if('serviceWorker' in navigator){
+if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register("/sw.js")
-		.then(function(){
+		.then(function () {
 			console.log("Service worker is registered!")
 		})
 }
 
 var videoPlayer;
-function startVideo(){
+function startVideo() {
 	//enables the camera, if possible
-	if('mediaDevices' in navigator){
+	if ('mediaDevices' in navigator) {
 		navigator.mediaDevices.getUserMedia({ video: true })
-			.then(function(stream){
+			.then(function (stream) {
 
 				//it will automatically ask for permission
 				//if the user denies, it will give back an error
@@ -71,7 +71,7 @@ function startVideo(){
 				$("#divSelfie").css("display", "block");
 
 			})
-			.catch(function(error){
+			.catch(function (error) {
 				//all errors here
 				console.log("There was an error", error);
 				//then show the image picker, because the camera doesn't work
@@ -80,44 +80,44 @@ function startVideo(){
 	}
 
 }
-function captureImage(){
+function captureImage() {
 	navigator.mediaDevices.getUserMedia({ video: true })
-			.then(function(stream){
-				var mediaStreamTrack = stream.getVideoTracks()[0];
-			  	var imageCapture = new ImageCapture(mediaStreamTrack);
-				var img = document.querySelector('#fotoselfie');
-				imageCapture.takePhoto()
-					.then(blob => {
-						img.src = URL.createObjectURL(blob);
-						img.onload = () => { URL.revokeObjectURL(this.src); }
-					})
-					.catch(error => console.error('takePhoto() error:', error));
-		});		
+		.then(function (stream) {
+			var mediaStreamTrack = stream.getVideoTracks()[0];
+			var imageCapture = new ImageCapture(mediaStreamTrack);
+			var img = document.querySelector('#fotoselfie');
+			imageCapture.takePhoto()
+				.then(blob => {
+					img.src = URL.createObjectURL(blob);
+					img.onload = () => { URL.revokeObjectURL(this.src); }
+				})
+				.catch(error => console.error('takePhoto() error:', error));
+		});
 }
 
-function pickImage(){
+function pickImage() {
 	stopStreaming();
 	$("#pickImage").css("display", "block");
 }
-function stopStreaming(){
-	if(videoPlayer){
+function stopStreaming() {
+	if (videoPlayer) {
 		videoPlayer.srcObject.getVideoTracks() //gives access to the running video streams of the object
-			.forEach(function(track){
+			.forEach(function (track) {
 				track.stop(); // stop all streams
 			})
 	}
 	$("#divSelfie").css("display", "none");
 }
 
-function captureImagePick(input){
+function captureImagePick(input) {
 	var reader = new FileReader();
-	reader.onload = function(e) {
-    	$("img").attr('src', e.target.result)
-    };
+	reader.onload = function (e) {
+		$("img").attr('src', e.target.result)
+	};
 	reader.readAsDataURL(input.target.files[0]);
 }
 
-$(function(){
+$(function () {
 	$("#selfieBtn").click(startVideo);
 	$("#captureBtn").click(captureImage);
 	$("#pickImgBtn").click(pickImage);
